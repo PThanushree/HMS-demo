fa3601536a239342e5973ea779953446ae3dec5c
ThanushreeP <thanu22052005@gmail.com>
2025-03-15 11:29:00 +0530

    Add dashboard features

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
+import './Dashboard.css'
+import Dashboard from './Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Dashboard />
   </StrictMode>,
 )
