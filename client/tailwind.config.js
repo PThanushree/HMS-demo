c4b3fbb9e7b0318eff1032cc5ef848eabea64540
ThanushreeP <thanu22052005@gmail.com>
2025-07-31 12:35:23 +0530

    Tailwind setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,10 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ✅ This includes all your components
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}", // ✅ This includes all your components
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
