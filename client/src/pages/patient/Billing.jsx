ece6a95e32b0f0879fca97a7cbdd7e4abe0cee51
Manasa patil <manasapatil226@gmail.com>
2025-07-31 13:54:12 +0530

    Frontend almost done

--- client/src/pages/patient/Billing.jsx
+++ client/src/pages/patient/Billing.jsx
@@ -68,16 +68,8 @@ const PaymentHistory = () => {
   };
 
   return (
-    <div className="container mx-auto mt-4">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-sky-700 text-3xl">Payment History</h1>
-        <button
-          className="bg-sky-700 hover:bg-sky-800 text-white px-4 py-2 rounded-md text-sm font-bold"
-          onClick={() => setShowForm(true)}
-        >
-          New Payment
-        </button>
-      </div>
+    <div className="container mx-auto mt-4 mt-6">
+      
 
       {showForm ? (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
@@ -185,6 +177,14 @@ const PaymentHistory = () => {
           </table>
         </div>
       )}
+      <div className="flex justify-between items-center mb-8 mt-6">
+        <button
+          className="bg-sky-700 hover:bg-sky-800 text-white px-4 py-2 rounded-md text-sm font-bold"
+          onClick={() => setShowForm(true)}
+        >
+          New Payment
+        </button>
+      </div>
     </div>
   );
 };
